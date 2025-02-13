4602fa921e4a76cab301ec590cce1689c41dde2a
mnougerede <mnougerede@gmail.com>
2025-02-13 11:32:13 +0000

    added some simple tests to InterpreterTests.cpp

--- tests/InterpreterTests.cpp
+++ tests/InterpreterTests.cpp
@@ -1,3 +1,28 @@
-//
-// Created by max on 13/02/25.
-//
+#include "gtest/gtest.h"
+#include "Interpreter.h"
+#include <any>
+#include <stdexcept>
+
+TEST(InterpreterTest, SimpleExpression) {
+    Interpreter interpreter;
+    // Given an expression "3 + 4;", we expect the result to be 7.
+    std::any result = interpreter.evaluate("3 + 4;");
+    int value = std::any_cast<int>(result);
+    EXPECT_EQ(value, 7);
+}
+
+TEST(InterpreterTest, ComplexExpression) {
+    Interpreter interpreter;
+    // Evaluate a more complex expression: 2 * (3 + 5) - 4 = 12.
+    std::any result = interpreter.evaluate("2 * (3 + 5) - 4;");
+    int value = std::any_cast<int>(result);
+    EXPECT_EQ(value, 12);
+}
+
+TEST(InterpreterTest, InvalidExpressionThrows) {
+    Interpreter interpreter;
+    // An invalid expression (e.g., missing operand) should throw an exception.
+    EXPECT_THROW({
+        interpreter.evaluate("3 + ;");
+    }, std::exception);
+}
